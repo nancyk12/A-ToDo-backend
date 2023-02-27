d8ac899303a7af01e78b316dcef5c8e84a382753
nancyk12 <nancyk12@hotmail.com>
2023-02-27 16:21:09 -0500

    adding npx express-generator files

--- app.js
+++ app.js
@@ -9,39 +9,39 @@ let { mongooseConnect } = require('./mongoose.js');
 mongooseConnect();
 
 
-const toDoListRouter = require('./routes/toDoList');
+const todoListRouter = require('./routes/todoList');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-//view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/toDoList', toDoListRouter);
-//catch 404 and forward to error handler
+app.use('/todoList', todoListRouter);
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
-//error handler
+// error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err. message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    //render error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
